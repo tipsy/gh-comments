dba703686b357f5bf1af70ecac643c7b9a76d729
Christian Rasmussen <christian.rasmussen@me.com>
2017-08-07 22:16:11 +0200

    replace babel with browserify (including improved error handling in gulpfile) (#3)
    * replace babel with browserify
    
    * improved error handling in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,35 @@
 const gulp = require('gulp');
-const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
 const cssmin = require('gulp-cssmin');
 const del = require('del');
+const tap = require('gulp-tap');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const buffer = require('vinyl-buffer');
+const rename = require('gulp-rename');
+const plumber = require('gulp-plumber');
 
 gulp.task('javascript', () => {
-    del('dist/js').then(() => gulp.src('src/js/gh-comments.js')
-        .pipe(babel({presets: ['es2015']})).on('error', logAndEmit)
-        .pipe(uglify()).on('error', logAndEmit)
+    del('dist/js').then(() => gulp.src('src/js/index.js')
+        .pipe(plumber())
+        .pipe(tap((file) => {
+            file.contents = browserify(file.path, {})
+                .transform(babelify, {presets: ['es2015']})
+                .bundle();
+            }))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(rename('gh-comments.js'))
         .pipe(gulp.dest('dist/js'))
     );
 });
 
 gulp.task('css', () => {
     del('dist/css').then(() => gulp.src('src/sass/gh-comments.scss')
-        .pipe(sass()).on('error', logAndEmit)
-        .pipe(cssmin()).on('error', logAndEmit)
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(cssmin())
         .pipe(gulp.dest('dist/css'))
     );
 });
@@ -27,9 +40,3 @@ gulp.task('watch', () => {
 });
 
 gulp.task('default', ['javascript', 'css']);
-
-// log and emit end (for restarting watch)
-function logAndEmit(e) {
-    console.log(e.toString());
-    this.emit('end');
-}
---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+'use strict';
+
+window.githubComments = require('./gh-comments');
